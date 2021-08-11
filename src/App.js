f35c126c1d1624d557ace095d8687416a585909a
Srijana Thulung <srijana.thulung123@gmail.com>
2021-08-11 10:48:25 +0545

    code for get method,try and catch is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import Tasks from './component/Tasks/Tasks';
 import NewTask from './component/NewTask/NewTask';
 
 
 function App() {
 
-  const [tasks, setTasks] = useState([])
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
   console.log(tasks);
   
+  const fetchTasks = async (taskText) => {
+    try {
+      const response = await fetch('https://realistic-example-7ef6f-default-rtdb.firebaseio.com/tasks.json');
+
+      if (!response.ok) {
+      throw new Error('Request failed')
+      }
+      const data = await response.json();
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({id:taskKey, text:data[taskKey].text})
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, [])
+
+  const taskAddHandler = () => {
+    setTasks((prevTasks) => prevTasks.concat(tasks));
+  }
+
   return (
     <React.Fragment>
 
-      <NewTask />
+      <NewTask onAddtask={taskAddHandler} />
       <Tasks
         items={tasks}
+        error={error}
+        onFetch={fetchTasks}
       />
       
     </React.Fragment>
