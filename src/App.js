7c27613bc6ea2e44456eb504c84a54b9446f9ffd
Srijana Thulung <srijana.thulung123@gmail.com>
2021-08-17 10:45:55 +0545

    adding custom hook logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React,{useEffect, useState,useCallback} from 'react';
 import Tasks from './component/Tasks/Tasks';
 import NewTask from './component/NewTask/NewTask';
 import useHttp from './hook/use-http';
@@ -9,21 +9,23 @@ function App() {
   const [tasks, setTasks] = useState([]);
   // console.log(tasks);
 
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
+ 
+  
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+  
       for (const taskKey in tasksObj) {
         loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text })
       }
-  }
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(
-    { url: 'https://realistic-example-7ef6f-default-rtdb.firebaseio.com/tasks.json' },
-    transformTasks
-  );
+      setTasks(loadedTasks);
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({ url: 'https://realistic-example-7ef6f-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks);
+  }, [fetchTasks]);
 
   const onAddTaskHandler = (task) => {
     console.log('onAddTask function is called succesfully');
--- src/hook/use-http.js
+++ src/hook/use-http.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState,useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = ( ) => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig,applyData) => {
         setIsLoading(false);
         setError(null);
     
@@ -29,7 +29,7 @@ const useHttp = (requestConfig, applyData) => {
           setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    },[]);
     
     return {
         isLoading: isLoading,
