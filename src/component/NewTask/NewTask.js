4168c862c6ee63cde162ade183ced0ba817a07b2
Srijana Thulung <srijana.thulung123@gmail.com>
2021-08-18 11:21:54 +0545

    custom hook is added to NewTask component

--- src/component/NewTask/NewTask.js
+++ src/component/NewTask/NewTask.js
@@ -1,37 +1,35 @@
 import React,{useState} from 'react';
 import TaskForm from './TaskForm.js';
 import Section from '../UI/Section.js';
+import useHttp from '../../hook/use-http.js';
 
 
 const NewTask = props => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+    const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+    
+    const createTask = (taskText,taskData) => {
+        const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+        const createdTask = { id: generatedId, text: taskText };
+        props.onAddTask(createdTask);
 
-    const enterTaskHandler =async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch('https://realistic-example-7ef6f-default-rtdb.firebaseio.com/tasks.json',
-                {
-                    method: 'POST',
-                    body: JSON.stringify({ text: taskText }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-            if (!response.ok) {
-                throw new Error('Request failed');
-            }
-            const data = await response.json();
-
-            const generatedId = data.name; // firebase-specific => "name" contains generated id
-            const createdTask = { id: generatedId, text: taskText };
-            props.onAddTask(createdTask)
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
     };
+
+    const enterTaskHandler = async (taskText) => {
+        sendTaskRequest(
+            {
+            url: 'https://realistic-example-7ef6f-default-rtdb.firebaseio.com/tasks.json',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: { text: taskText }
+        },
+            //function that takes the response data and does something with it
+            //in custom hook response data is taking only one argument so we are using bind()
+            createTask.bind(null,taskText)
+        );
+    };
+    
     return (
         <Section>
             <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
